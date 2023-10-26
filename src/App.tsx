277277f3ae9397ae0ec680a676eab90ee46c80c6
Carl Fredrik Ahl <carlfredrikahl@gmail.com>
2023-10-26 20:57:14 +0200

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import InputField from './components/InputField';
 
 function App() {
   return (
     <div className="App">
       <span className="heading">Taskify</span>
+      <InputField></InputField>
+
     </div>
   );
 }
