2e57425cf57726301ada98f88d33896a40e2709f
Carl Fredrik Ahl <carlfredrikahl@gmail.com>
2023-10-27 18:58:02 +0200

    Can add text

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import {Todo} from "./components/model";
 
 function App() {
+
+  const[todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if(todo) {
+      setTodos([...todos, {id:Date.now(), todo, isDone:false}]);
+      setTodo("");
+    }
+  };
+
+  console.log(todos);
+
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField></InputField>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+
+      {todos.map((t) => (
+        <li>{t.todo}</li>
+      ))}
 
     </div>
   );
